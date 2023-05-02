239acf9c8fc13557a4f12fe53a3fe3e5bacb6677
issa1807 <isaprz52@gmail.com>
2023-05-02 13:55:15 -0600

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+for num_list in range(1, 101):
+    print(num_list)
+    if num_list % 3 == 0:
+        print ("fizz")
+    elif num_list % 5 == 0:
+         print ("buzz")
+    else:
+       print(num_list)
+
+
